62f7192c833fa91bcf4c2493f1ded034f16e35e2
Brian Sturgis <sturujisu@gmail.com>
2021-05-17 15:21:33 -0700

    update project with basic styling and button functionality

--- frontend/components/Commands.js
+++ frontend/components/Commands.js
@@ -1,4 +1,72 @@
+import styled from 'styled-components';
 import socket from '../socket';
+
+const CommandGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1.25fr 1fr;
+  grid-template-rows: repeat(3, 1fr);
+  border: 1px solid black;
+  grid-gap: 3px;
+  button {
+    text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.05);
+    border: 0;
+    background: #fe2c70;
+    border: 4px solid transparent;
+    color: white;
+    font-size: 1rem;
+    position: relative;
+    &:active {
+      top: 2px;
+    }
+    &:focus {
+      outline: 0;
+      border-color: #ffc600;
+    }
+    &.takeoff {
+			font-size: 2rem;
+      background: #41c7ff;
+    }
+    &.land {
+			font-size: 2rem;
+      background: #00ff00;
+    }
+    &.emergency {
+      background: orange;
+      text-transform: uppercase;
+      color: black;
+    }
+    &.rotate {
+      background: #00fff9;
+      color: black;
+    }
+    &.height {
+      background: #fff;
+      color: black;
+    }
+    span.symbol {
+      display: block;
+      font-size: 2rem;
+      font-weight: 400;
+    }
+  }
+  .center {
+    display: grid;
+    grid-gap: 3px;
+    grid-template-columns: 1fr 1fr;
+    button:last-child {
+      grid-column: span 2;
+    }
+  }
+  h2 {
+    grid-column: 1 / -1;
+    background: #ffc600;
+    margin: 0;
+    font-size: 3rem;
+    text-align: center;
+    padding: 0.5rem;
+    color: black;
+  }
+`;
 
 function sendCommand(command) {
   return function() {
@@ -11,13 +79,13 @@ const amount = 100;
 const Commands = () => (
   <CommandGrid>
     <button className="rotate" onClick={sendCommand('ccw 90')}>
-      <span className="symbol">⟲</span> 90°
+      <span className="symbol">rotate CCW</span> 90°
     </button>
     <button onClick={sendCommand(`forward ${amount}`)}>
       <span className="symbol">↑</span> forward {amount}cm
     </button>
     <button className="rotate" onClick={sendCommand('cw 15')}>
-      <span className="symbol">⟳</span> 15°
+      <span className="symbol">rotate CW</span> 15°
     </button>
     <button onClick={sendCommand(`left ${amount}`)}>
       <span className="symbol">←</span> left {amount}cm
@@ -30,7 +98,7 @@ const Commands = () => (
         Land
       </button>
       <button className="emergency" onClick={sendCommand('emergency')}>
-        !! emergency !!
+        <h2>OH SHIT</h2>
       </button>
     </div>
     <button onClick={sendCommand(`right ${amount}`)}>
@@ -38,20 +106,22 @@ const Commands = () => (
       right {amount}cm
     </button>
     <button className="height" onClick={sendCommand(`up ${amount}`)}>
-      <span className="symbol">⤒</span> {amount}cm
+      <span className="symbol">⤒ up</span> {amount}cm
     </button>
     <button onClick={sendCommand(`back ${amount}`)}>
       <span className="symbol">↓</span> back {amount}cm
     </button>
     <button className="height" onClick={sendCommand(`down ${amount}`)}>
-      <span className="symbol">⤓</span> {amount}cm
+      <span className="symbol">⤓ down</span> {amount}cm
     </button>
-   
     <button onClick={sendCommand('flip l')}>Flip Left</button>
     <button onClick={sendCommand('flip r')}>Flip Right</button>
     <button onClick={sendCommand('flip b')}>Flip Back</button>
     <button onClick={sendCommand('flip f')}>Flip Forward</button>
-    
+    <button onClick={sendCommand('go 25 25 25 25')}>Go 25 25 25 25</button>
+    <button onClick={sendCommand('curve 100 100 100 150 250 350 50')}>
+      Curve!
+    </button>
   </CommandGrid>
 );
 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,38 @@
 import DroneState from '../components/DroneState';
 import Commands from '../components/Commands';
 
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: white;
+    /* font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; */
+    font-family: 'Operator Mono', monospace;
+    font-weight: 900;
+    font-size: 1rem;
+    background:#1C6CFD;
+    color: white;
+  }
+  * {
+    font-family: 'Operator Mono', monospace;
+    box-sizing: border-box;
+  }
+  h2 {
+    text-align: center;
+  }
+`;
+
+const PageStyles = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+`;
+
 const IndexPage = () => (
-  <div>
-		<h2>hello drone</h2>
+  <PageStyles>
+    <GlobalStyle />
     <Commands />
-  </div>
+    {/* <DroneState /> */}
+  </PageStyles>
 );
 
 export default IndexPage;
